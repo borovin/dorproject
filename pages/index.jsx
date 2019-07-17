8c3da63185d0544d82861a89a5784e19ccb8d3f4
Andrew Borovin <a@borovin.com>
2019-07-17 15:37:16 +0300

    v1.0.0

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home page</title>
+        <link rel="stylesheet" href="/static/main.css" />
+      </Head>
+      <h1>Home page</h1>
+    </>
+  );
+}
+
+export default Home;
